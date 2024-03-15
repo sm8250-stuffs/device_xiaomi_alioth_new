45c85a5f1d7d103902a00425160b3650dced440f
levi3609 <nazmulhasan3609@gmail.com>
2024-03-15 15:00:00 +0000

    Alioth: disable faceunlock & include gapps

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -15,6 +15,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
+# Google Basic apps
+$(call inherit-product-if-exists, vendor/gapps/Android.mk)
+
 PRODUCT_NAME := lineage_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
@@ -26,7 +29,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Rom Specific Flags
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP  := true
 
